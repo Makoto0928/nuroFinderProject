aa90d05c133f1b08cb400ac23d5711133e5a5b20
Tsuruoka Makoto <makotty0928@gmail.com>
2019-12-07 16:06:36 +0900

    20191207_nuroHouseFinder

--- src/main/java/crawlerForNuroHouse/NuroHouseCrawler.java
+++ src/main/java/crawlerForNuroHouse/NuroHouseCrawler.java
@@ -2,6 +2,7 @@ package crawlerForNuroHouse;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -10,6 +11,8 @@ import org.openqa.selenium.support.ui.Select;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class NuroHouseCrawler {
@@ -47,9 +50,40 @@ public class NuroHouseCrawler {
 						townNumber = Integer.toString(j);
 						dropDownTown.selectByValue(townNumber);
 //						driver.findElement(By.id("buildings-list"));
-						String address = driver.findElement(By.xpath("//*[@id=\"items\"]/p[1]/span[1]")).getText();
-						String buildingName = driver.findElement(By.xpath("//*[@id=\"items\"]/p[1]/span[2]")).getText();
-						pw.println(buildingName + "," + address);
+						String address = "";
+						String buildingName = "";
+						//TODO .findElements(By.cssSelector("p.item > span")　でNotFoundも含めてデータを取得すればよい。foreachで回せば何物件あるか知らなくてもいけるだろう。
+						String judgmentFoExist = driver.findElement(By.cssSelector("#items > p")).getText();
+						System.out.println(judgmentFoExist);
+						if (judgmentFoExist.contains("導入済みマンション")) {
+							address = "NotFound";
+							buildingName = "NotFound";
+							System.out.println(buildingName + " " + address);
+							pw.println(buildingName + "," + address);
+						}	else {
+							List<WebElement> buildingNumber = driver.findElements(By.id("items"));
+							for (WebElement building : buildingNumber) {
+								List<WebElement> buildingInfos = building.findElements(By.cssSelector("p.item > span"));
+								List<String> dataStock = new ArrayList<String>();
+								for (WebElement buildingInfo : buildingInfos) {
+									dataStock.add(buildingInfo.getText());
+								}
+								address = dataStock.get(0);
+								buildingName = dataStock.get(1);
+								pw.println(buildingName + "," + address);
+							}
+							
+//							for (int k = 1; k <= buildingNumber.size(); k++) {
+//								System.out.println(k);
+//								String xPathForAddress = "//*[@id=\"items\"]/p[" + k + "]/span[1]";
+//								System.out.println(xPathForAddress);
+//								String xPathForBuildingName = "//*[@id=\"items\"]/p[" + k + "]/span[2]";
+//								System.out.println(xPathForBuildingName);
+//								address = driver.findElement(By.xpath(xPathForAddress)).getText();
+//								buildingName = driver.findElement(By.xpath(xPathForBuildingName)).getText();
+//								pw.println(buildingName + "," + address);
+//							}
+						}
 						try {
 							Thread.sleep(5);
 						}	catch (InterruptedException ie) {
@@ -65,7 +99,7 @@ public class NuroHouseCrawler {
 					}
 				}
 				pw.close();
-				
+				System.out.println("FINISH!");
 			}	catch (Exception e) {
 				System.out.println("==========Crawling Error ==========");
 				e.printStackTrace();
