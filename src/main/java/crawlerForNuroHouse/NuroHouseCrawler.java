9930cda37de7c9af907521576f6647827c3e5956
Tsuruoka Makoto <makotty0928@gmail.com>
2019-12-08 19:08:01 +0900

    20191208_nuroHouseFinder_Completed_1

--- src/main/java/crawlerForNuroHouse/NuroHouseCrawler.java
+++ src/main/java/crawlerForNuroHouse/NuroHouseCrawler.java
@@ -1,5 +1,11 @@
 package crawlerForNuroHouse;
 
+import com.google.maps.GeoApiContext;
+import com.google.maps.GeocodingApi;
+import com.google.maps.GeocodingApiRequest;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.GeocodingResult;
+import com.google.maps.model.LatLng;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,22 +16,40 @@ import org.openqa.selenium.support.ui.Select;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
+import java.util.ResourceBundle;
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class NuroHouseCrawler {
+	
+	private static String goeCodingApiKey() {
+		ResourceBundle rb = ResourceBundle.getBundle("googleGeocodingApi");
+		return rb.getString("apiKey");
+	}
+	private static GeoApiContext context = new GeoApiContext.Builder()
+			.apiKey(goeCodingApiKey())
+			.build();
+	
+	
 	public static void main(String[] args) {
 		
 		try {
+			Scanner scanner = new Scanner(System.in);
 			FileWriter file		= null;
 			PrintWriter pw		= null;
-			file = new FileWriter("C:\\Users\\makot\\Documents\\nuroHouse\\nuroHouseFindResultAddress.csv", true);
+			System.out.print("csvFilePathForAddress: ");
+			String csvFilePathForAddress = scanner.nextLine();
+			file = new FileWriter(csvFilePathForAddress, true);
 			pw = new PrintWriter(new BufferedWriter(file));
-			FileWriter file1	= null;
-			PrintWriter pw1		= null;
-			file1 = new FileWriter("C:\\Users\\makot\\Documents\\nuroHouse\\nuroHouseFindResultBuildingName.csv", true);
-			pw1 = new PrintWriter(new BufferedWriter(file1));
+//			FileWriter file1	= null;
+//			PrintWriter pw1		= null;
+//			System.out.print("csvFilePathForBuildingName: ");
+//			String csvFilePathForBuildingName = scanner.nextLine();
+//			file1 = new FileWriter(csvFilePathForBuildingName, true);
+//			pw1 = new PrintWriter(new BufferedWriter(file1));
 			
 			try {
 				System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\ChromeDriver78\\chromedriver.exe");
@@ -41,7 +65,8 @@ public class NuroHouseCrawler {
 				Select dropDownCity = new Select(driver.findElement(By.id("city-list")));
 				String cityNumber = "";
 				int townValue[] = {115, 98, 117, 157, 68, 108, 104, 156, 130, 88, 216, 277, 80, 85, 139, 83, 113, 52, 134, 202, 269, 155, 201};
-				
+				pw.println("Address,Lat,Lng");
+				System.out.println("Address,Lat,Lng");
 				for (int i = 0; i < 23; i++) {
 					cityNumber = Integer.toString(i);
 					dropDownCity.selectByValue(cityNumber);
@@ -52,19 +77,23 @@ public class NuroHouseCrawler {
 						townNumber = Integer.toString(j);
 						dropDownTown.selectByValue(townNumber);
 						String address = "";
-						String buildingName = "";
+//						String buildingName = "";
 						List<WebElement> buildingList = driver.findElements(By.cssSelector("#items > p > span.label"));
 						for (WebElement building : buildingList) {
 							address = building.getText();
-							System.out.println(address);
-							pw.println(address);
+							GeocodingResult[] results = getResults(address);
+							if (results != null && results.length > 0) {
+								LatLng latLng = results[0].geometry.location;
+								System.out.println("Address: " + address + ", " + "Lat: " + latLng.lat + ", " + "Lng: " + latLng.lng);
+								pw.println(address + "," + latLng.lat + "," + latLng.lng);
+							}
 						}
-						List<WebElement> buildingNameList = driver.findElements(By.cssSelector("#items > p > span.selectable"));
-						for (WebElement buildingNameElement : buildingNameList) {
-							buildingName = buildingNameElement.getText();
-							System.out.println(buildingName);
-							pw1.println(buildingName);
-						}
+//						List<WebElement> buildingNameList = driver.findElements(By.cssSelector("#items > p > span.selectable"));
+//						for (WebElement buildingNameElement : buildingNameList) {
+//							buildingName = buildingNameElement.getText();
+//							System.out.println(buildingName);
+//							pw1.println(buildingName);
+//						}
 						
 						try {
 							Thread.sleep(5);
@@ -91,6 +120,33 @@ public class NuroHouseCrawler {
 		}	catch (Exception ex) {
 			System.out.println("+++++ERROR+++++");
 			ex.printStackTrace();
+		}
+	}
+	
+	
+	/** Google Geocoding API handling */
+	public static GeocodingResult[] getResults(String address) throws ApiException, InterruptedException, IOException {
+		GeocodingApiRequest req = GeocodingApi.newRequest(context)
+				.address(address)
+				.language("ja");
+		try {
+			GeocodingResult[] results = req.await();
+			if (results == null || results.length == 0) {
+				System.out.println("zero results.");
+			}
+			return results;
+		}	catch (ApiException e) {
+			System.out.println("geocode failed.");
+			e.printStackTrace();
+			throw e;
+		}	catch (IOException e) {
+			System.out.println("IOException");
+			e.printStackTrace();
+			throw e;
+		}	catch (InterruptedException e) {
+			System.out.println("InterruptedException");
+			e.printStackTrace();
+			throw e;
 		}
 	}
 }
